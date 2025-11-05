969f8508e8264aa53b5b6689c15f30a0e22dbd94
yixiangt <yixiangt@seas.upenn.edu>
2025-11-05 16:33:36 -0500

    update WordRecommender.java by changing the computation of similarit

--- WordRecommender.java
+++ WordRecommender.java
@@ -19,22 +19,23 @@ public class WordRecommender {
 
     private int leftSimilarity(String a, String b) {
         int n = Math.min(a.length(), b.length());
-        int cnt = 0;
+        int count = 0;
         for (int i = 0; i < n; i++) {
-            if (a.charAt(i) == b.charAt(i)) cnt++;
+            if (a.charAt(i) == b.charAt(i)) count++;
         }
-        return cnt;
+        return count;
     }
 
     private int rightSimilarity(String a, String b) {
         int i = a.length() - 1;
         int j = b.length() - 1;
-        int cnt = 0;
-        while (i >= 0 && j >= 0) {
-            if (a.charAt(i) == b.charAt(j)) cnt++;
-            i--; j--;
-        }
-        return cnt;
+        int count = 0;
+        if (i >= 0 && j >= 0) do {
+            if (a.charAt(i) == b.charAt(j)) count++;
+            i--;
+            j--;
+        } while (i >= 0 && j >= 0);
+        return count;
     }
     // You can of course write other methods as well.
   }
