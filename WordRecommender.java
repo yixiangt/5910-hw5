1e26299ae5dafb088fbc6d6c3acee9886322ff28
yixiangt <yixiangt@seas.upenn.edu>
2025-11-05 16:20:09 -0500

    update WordRecommender.java by adding methods: getSimilarity, leftSimilarity and rightSimilarity

--- WordRecommender.java
+++ WordRecommender.java
@@ -2,19 +2,39 @@ import java.util.ArrayList;
 
 public class WordRecommender {
 
-    public WordRecommender(String dictionaryFile) {    
+    public WordRecommender(String dictionaryFile) {
       // TODO: change this!
-    }
-  
-    public double getSimilarity(String word1, String word2) {
-      // TODO: change this!
-      return 0.0;
     }
   
     public ArrayList<String> getWordSuggestions(String word, int tolerance, double commonPercent, int topN) {
       // TODO: change this!
       return null;
     }
-  
+
+    public double getSimilarity(String a, String b) {
+        int left = leftSimilarity(a, b);
+        int right = rightSimilarity(a, b);
+        return (left + right) / 2.0;
+    }
+
+    private int leftSimilarity(String a, String b) {
+        int n = Math.min(a.length(), b.length());
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            if (a.charAt(i) == b.charAt(i)) cnt++;
+        }
+        return cnt;
+    }
+
+    private int rightSimilarity(String a, String b) {
+        int i = a.length() - 1;
+        int j = b.length() - 1;
+        int cnt = 0;
+        while (i >= 0 && j >= 0) {
+            if (a.charAt(i) == b.charAt(j)) cnt++;
+            i--; j--;
+        }
+        return cnt;
+    }
     // You can of course write other methods as well.
   }
