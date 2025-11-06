bcc5fc00e623d0a08889f86a911edecddeecc1e0
lingzhuyu <yu36@seas.upenn.edu>
2025-11-06 16:38:43 -0500

    Prompt for valid filename, print error o.w

--- SpellChecker.java
+++ SpellChecker.java
@@ -12,13 +12,26 @@ public class SpellChecker {
   
     public void start() {
       // TODO: Complete the body of this method, as necessary.
+        String dictFile = getValidFilename(Util.DICTIONARY_PROMPT);
       inputReader.close();  // DO NOT MODIFY - must be the last line of this method!
     }
 
+    private String getValidFilename(String prompt) {
+        while (true) {
+            System.out.printf(prompt);
+            String filename = inputReader.nextLine().trim();
+            if (fileExists(filename)) {
+                return filename;
+            } else {
+                System.out.printf(Util.FILE_OPENING_ERROR);
+            }
+        }
+    }
     private boolean fileExists(String filename) {
         File f = new File(filename);
         return f.exists() && f.isFile();
     }
+
 
 
     // You can of course write other methods as well.
